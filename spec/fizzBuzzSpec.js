a35b0e88d63cfeed5b36969ee8c0cbddc13408bd
malin patel <malin.patel@icloud.com>
2016-12-05 16:17:01 +0000

    Divisible by 3 & 5 tests added

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,11 +1,32 @@
 describe("FizzBuzz", function(){
 
   it("is divisible by three", function(){
-    expect(is_divisible_by_3(3)).toEqual(true);
+    expect(is_divisible_by_3(3)).toEqual('Fizz');
   });
 
+  it("is not divisible by three", function(){
+    expect(is_divisible_by_3(4)).toEqual(4);
+  });
 
+  it("is divisible by five", function(){
+    expect(is_divisible_by_5(5)).toEqual('Buzz');
+  });
 
+  it("is not divisible by five", function(){
+    expect(is_divisible_by_5(6)).toEqual(6);
+  });
+
+  it("is divisible by three and five", function(){
+    expect(is_divisible_by_15(15)).toEqual('FizzBuzz');
+  });
+
+  it("is not divisible by three and five", function(){
+    expect(is_divisible_by_15(16)).toEqual(16);
+  });
+
+  it("should test for arbitrary divisibility", function(){
+    expect(is_divisible_by_number(3,3)).toEqual(true);
+  });
 
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,35 @@
 function is_divisible_by_3(number){
   if (number % 3 === 0) {
+    return 'Fizz';
+  }
+  else {
+    return number;
+  }
+}
+
+function is_divisible_by_5(number){
+  if (number % 5 === 0) {
+    return 'Buzz';
+  }
+  else {
+    return number;
+  }
+}
+
+function is_divisible_by_15(number){
+  if (number % 15 === 0) {
+    return 'FizzBuzz';
+  }
+  else {
+    return number;
+  }
+}
+
+function is_divisible_by_number(number,divisor){
+  if (number % divisor === 0) {
     return true;
+  }
+  else {
+    return false;
   }
 }
