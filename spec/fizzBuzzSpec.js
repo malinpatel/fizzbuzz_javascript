3b03f6d237201cabd42b5e953394e634e1dd62da
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-05 16:00:38 +0000

    First Commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,11 @@
+describe("FizzBuzz", function(){
+
+  it("is divisible by three", function(){
+    expect(is_divisible_by_3(3)).toEqual(true);
+  });
+
+
+
+
+
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,5 @@
+function is_divisible_by_3(number){
+  if (number % 3 === 0) {
+    return true;
+  }
+}
